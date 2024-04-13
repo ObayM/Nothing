492bba956552dd0e0cc2b86b92df3da18daa6ef1
obay.zamir <obay.zamir@gmail.com>
2024-04-13 04:34:53 +0000

    Setup Rest framework || Create accounts app

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
+    'rest_framework.authtoken'
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:5500',
+# ]
